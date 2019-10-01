b5aa365fa64110ca6f2e6f562d3d6dbf01350e6b
Gregory <itsgregreed@gmail.com>
2019-10-01 13:46:34 -0400

    chane

--- src/main/java/com/collabera/three/animalshelter/repository/ShelterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.collabera.three.animalshelter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.collabera.three.animalshelter.model.Shelter;
-
-public interface ShelterRepo extends JpaRepository<Shelter, Integer> {
-
-}
